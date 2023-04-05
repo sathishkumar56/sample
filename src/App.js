01926d36bfb91ae65c5b60498de414f19bef8307
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-05 17:29:56 +0530

    xx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Login } from 'simple-component-library-sat';
+import Carousel1 from 'simple-component-library-sat/dist/caurosel/caurosel';
+// import Carousel1 from './caurosel/caurosel';
 import './App.css';
+import Img1 from './images/s1.jpg';
+import Img2 from './images/s2.jpg';
+import Img3 from './images/s3.jpg';
+import Img4 from './images/s4.jpg';
 
 function App() {
+  const images = [
+    Img1,Img2, Img3, Img4
+    
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div >
+        <Carousel1 images={images} />
+        {/* < Carousel1 /> */}
+      </div>
+      
+      <div>
+        <Login />
+      </div>
+    </>
   );
 }
 
---
+++ src/caurosel/caurosel.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+
+function Carousel1({images}) {
+    debugger
+    if (images == undefined) return;
+    debugger
+    console.log(images);
+    return (
+        <div>
+            <Carousel
+                infiniteLoop={true}
+                autoPlay={true}
+                showStatus={false}
+                showArrows={true}
+                showThumbs={false}
+                interval={1500}
+            >
+                {images.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`image${index + 1}`} />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
+}
+export default Carousel1;
